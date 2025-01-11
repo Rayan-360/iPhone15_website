024cac2b45ce02004c3df68fe2d6264583d469b6
Rayan-360 <skrayan0017@gmail.com>
2025-01-11 15:38:21 +0530

    Completed navbar and hero component

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Highlights = () => {
+  return (
+    <section id="highlights" className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div></div>
+
+    </section>
+  )
+}
+
+export default Highlights;
