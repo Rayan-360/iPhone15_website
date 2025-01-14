72543b33b072fc04cf69645f88d5be446bbc26ee
Rayan-360 <skrayan0017@gmail.com>
2025-01-14 11:06:29 +0530

    Completed 3d model section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,9 @@ const Hero = () => {
         </div>
       </div>
       <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
-        <a href="#highlights" className='btn'>Buy</a>
+        <a href="#highlights" 
+        onClick={() => sco}
+        className='btn'>Buy</a>
         <p>From $199/month or $999</p>
       </div>
     </section>
